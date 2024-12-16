fe79cb08b976b2f57ceb9b355176fe2d50fc20e9
Ryu Orlando Tamin <ryutamin05@gmail.com>
2024-12-16 12:10:27 +0700

    fix: changed float to complex in diagonalizable

--- backend/app/schemas/diagonalizable_schema.py
+++ backend/app/schemas/diagonalizable_schema.py
@@ -1,10 +1,19 @@
 from pydantic import BaseModel
 from typing import List
+import orjson
 
 class DiagonalizableInput(BaseModel):
-  matrix: List[List[float]]
+  matrix: List[List[complex]]
+  class Config:
+    json_dumps = orjson.dumps
+    json_loads = orjson.loads
 
 class DiagonalizableResponse(BaseModel):
-  eigenvalues: List[float]
-  eigenvectors: List[List[float]]
+  eigenvalues: List[complex]
+  eigenvectors: List[List[complex]]
   is_diagonalizable: bool
+  class Config:
+    json_dumps=lambda obj, *, default: orjson.dumps(
+        obj, option=orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_NON_STR_KEYS
+      ).decode("utf-8"),
+    json_loads=orjson.loads
--- backend/app/services/diagonalizable_service.py
+++ backend/app/services/diagonalizable_service.py
@@ -1,26 +1,31 @@
 import numpy as np
-from ..schemas import DiagonalizableResponse, DiagonalizableInput
+from ..schemas import DiagonalizableInput, DiagonalizableResponse
 
-def check_diagonalizable(matrix: DiagonalizableInput) -> DiagonalizableResponse:
-  matrix = np.array(matrix)
+def check_diagonalizable(input_data: DiagonalizableInput) -> DiagonalizableResponse:
+  matrix = np.array(input_data)
   eigenvalues, eigenvectors = np.linalg.eig(matrix)
 
   tolerance = 1e-10
+  #gilangkan bagian imajiner jika sangat kecil
+  eigenvalues = [
+    complex(val.real, val.imag if abs(val.imag) >= tolerance else 0)
+    for val in eigenvalues
+  ]
+  eigenvectors = [
+    [complex(val.real, val.imag if abs(val.imag) >= tolerance else 0) for val in row]
+    for row in eigenvectors
+  ]
 
-  #hilangkan bagian imajiner jika sangat kecil
-  eigenvalues = [val.real if abs(val.imag) < tolerance else val for val in eigenvalues]
-  eigenvectors = [[val.real if abs(val.imag) < tolerance else val for val in row] for row in eigenvectors]
-  
   #algebraic multiplicity adalah jumlah kemunculan suatu eigenvalue dalam matrix
-  algebraic_multiplicity = {val: eigenvalues.count(val) for val in set(eigenvalues)} #maksud set untuk ngilangin duplikat
-  
+  algebraic_multiplicity = {val: eigenvalues.count(val) for val in set(eigenvalues)}
+
   #geometric multiplicity adalah rank dari matrix yang sudah diubah ke basis eigenvalue
   geometric_multiplicity = {}
   for val in set(eigenvalues):
     eigenspace_matrix = matrix - val * np.eye(matrix.shape[0])
     rank = np.linalg.matrix_rank(eigenspace_matrix)
     geometric_multiplicity[val] = matrix.shape[0] - rank
-  
+
   #akan diagonalizable jika algebraic multiplicity = geometric multiplicity
   is_diagonalizable = all(
     algebraic_multiplicity[val] == geometric_multiplicity[val]
@@ -28,7 +33,7 @@ def check_diagonalizable(matrix: DiagonalizableInput) -> DiagonalizableResponse:
   )
 
   return DiagonalizableResponse(
-      eigenvalues=eigenvalues,
-      eigenvectors=eigenvectors,
-      is_diagonalizable=is_diagonalizable
+    eigenvalues=eigenvalues,
+    eigenvectors=eigenvectors,
+    is_diagonalizable=is_diagonalizable,
   )
