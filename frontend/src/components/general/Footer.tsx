0460717d98b3ea5e05723634e65b548ddd38d851
DavinTanaya <davin.tanaya@gmail.com>
2024-12-28 00:16:32 +0700

    fix: namanya vania salah

--- frontend/src/components/general/Footer.tsx
+++ frontend/src/components/general/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
     <footer className="footer bg-transparent text-center p-3 border-t">
-      <p>Davin Tanaya • Dean Febrio • Ryu Orlando Tamin • Vania Wyanet Tumbaleka</p>
+      <p>Davin Tanaya • Dean Febrio • Ryu Orlando Tamin • Vania Wyanet Tumbelaka</p>
       <p>© 2024 Copyright. All right reserved.</p>
     </footer>
   )
